ceb79a5ef768c45a9f1f58d163ec1c5db62c325c
Wangtingyang <100877461+Wangtingyang@users.noreply.github.com>
2022-07-05 04:08:07 +0300

    Add files via upload

---
+++ Task01.java
@@ -0,0 +1,31 @@
+package test.Task2;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Task01 {
+    public static void main(String[] args) {
+        ArrayList<Integer> A = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i <= N; i++) {
+            A.add(i);
+        }
+        int k = sc.nextInt();
+        int sum = 0;
+        if(k <= 0){
+            System.out.println("wrong number k");
+        }
+        else {
+            System.out.println(k);
+            for(int i : A){
+                if(i%k == 0){
+                    sum += i;
+                }
+            }
+        }
+        System.out.println("Sum of elements: "+sum);
+    }
+}
